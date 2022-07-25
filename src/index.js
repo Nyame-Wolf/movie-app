7a967bdcef8442d3c4a4a5288cac7c0d4aa86f92
Nyame-Wolf <mumenyam@gmail.com>
2022-07-25 23:03:39 +0300

    Modify style.css
    adjust index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Javascript Group Capstone']);
 
   return element;
 }
