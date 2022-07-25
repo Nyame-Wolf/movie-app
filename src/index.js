6de2262aad5756e68511ef0a52322a0935166674
Romina <romina.pati@gmail.com>
2022-07-25 15:22:07 -0400

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import './style.css'
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,34 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: '[name]-[hash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
