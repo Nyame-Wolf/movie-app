20f30204ccf39aa62e4fa59bbefde59025e79304
Nyame-Wolf <mumenyam@gmail.com>
2022-07-28 01:06:03 +0300

    Fix api to display cards
    Add styles to table that holds cards
    
    Changed post from .json() to text()

--- src/modules/commentModal/postComments.js
+++ src/modules/commentModal/postComments.js
@@ -1,6 +1,9 @@
 import { baseUrl, appId } from '../api.js';
 
 const postComments = async (id, username, comment, creationDate) => {
+  console.log({
+    id, username, comment, creationDate,
+  });
   const response = await fetch(`${baseUrl}/${appId}/comments`, {
     method: 'POST',
     headers: {
@@ -13,7 +16,7 @@ const postComments = async (id, username, comment, creationDate) => {
       creation_date: creationDate,
     }),
   });
-  const jsonResponse = await response.json();
+  const jsonResponse = await response.text();
   return jsonResponse;
 };
 export default postComments;
